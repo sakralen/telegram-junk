e4892f21d80f5b38f3a2a92d3b9f22dd4a6f45e8
bumashka <90649137+bumashka@users.noreply.github.com>
2024-05-09 00:28:46 +0300

    initial commit into branch terraform + ansible

--- src/main/java/com/example/movierecommendationbot/repository/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.movierecommendationbot.repository;
-
-import com.example.movierecommendationbot.model.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-}
