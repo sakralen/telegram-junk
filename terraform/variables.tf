15bb7456061185329cec025786b4652e74a5ba13
bumashka <90649137+bumashka@users.noreply.github.com>
2024-05-15 22:02:03 +0300

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "image_name" {
+  default = "ubuntu-22.04"
+}
+
+variable "flavor_name" {
+  default = "m1.small"
+}
+
+variable "key_pair" {
+  default = "buldakova-1"
+}
+
+variable "security_group" {
+  default = "default"
+}
+
+variable "network_name" {
+  default = "sutdents-net"
+}
